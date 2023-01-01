94cb5fb0fb838b8dacbd5a1c6295c914660252cb
Shankar Soni <77094387+shankarsoni@users.noreply.github.com>
2022-12-31 23:23:23 -0700

    my first gradle project

---
+++ src/main/java/com/mywork/demo/gradle/DemoClass.java
@@ -0,0 +1,8 @@
+package com.mywork.demo.gradle;
+
+public class DemoClass {
+    public static void main(String[] args) {
+        System.out.println(" -DemoClass.main- ");
+        System.out.println("Hello world");
+    }
+}
---
+++ src/test/java/com/mywork/demo/gradle/DemoClassTest.java
@@ -0,0 +1,13 @@
+package com.mywork.demo.gradle;
+
+import org.junit.Test;
+
+//import static org.junit.Assert.*;
+
+public class DemoClassTest {
+
+    @Test
+    public void main() {
+        DemoClass.main(new String[0]);
+    }
+}
